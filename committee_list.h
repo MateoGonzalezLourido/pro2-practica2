8619f37a134e289b600268f9f9b1b22738f5eb5e
MateoGonzalezLourido <mlourido1467@gmail.com>
2026-03-29 17:13:13 +0200

    añadido funciones commitee

--- committee_list.h
+++ committee_list.h
@@ -10,9 +10,9 @@
 #include "project_list.h"
 #include "types.h"
 
-/* Write your code here... */
+// estructuras
 #define MAX_COMITES 10
-#define NULLC -1
+#define NULLC (-1)
 typedef struct {
   tCommitteeName committeeName;
   tNumVotes totalEvaluators;
@@ -29,6 +29,30 @@ struct Lista {
 typedef int tPosC;
 typedef tPosC tListC;
 
+// funciones
 
-
-
+// funciones del tipo tList
+void createEmptyListC(tListC *L); // crear lista (devuelve un tList), (void)
+                                  // indica "no recibe argumentos"
+bool isEmptyListC(tListC L);      // vacia?; recibe la lista
+tPosC firstC(tListC L); // devolver primer indice ocupado;recibe la lista
+tPosC lastC(tListC L);  // devolver ultimo indice ocupado; recibe la lista
+tPosC nextC(
+    tPosC indice,
+    tListC L); // devolver siguiente indice ocupado;recibe indice y lista
+tPosC previousC(
+    tPosC indice,
+    tListC L); // devolver anterior indice ocupado; recibe indice y lista
+bool insertItemC(tItemC data, tPosC indice,
+                 tListC *L); // añadir elemento;recibe info insertar, indice
+                             // donde se cambia, lista usar
+void deleteAtPositionC(tPosC indice,
+                       tListC *L); // borrar proyecto; recibe indice donde
+                                   // borrar y la lista donde borrar
+tItemC getItemC(tPosC indice, tListC L); // recuperar info proyecto
+void updateItemC(tItemC data, tPosC indice,
+                 tListC *L); // modificar lista; recibe cambiar, indice lista
+                             // actualizar, lista usar
+tPosC findItemC(
+    tProjectName data,
+    tListC L); // buscar indice proyecto; recibe info proyecto, lista usar
