3f10223f1d76ef9ca92d25b65cb1b82e84d797e7
MateoGonzalezLourido <mlourido1467@gmail.com>
2026-03-29 17:02:40 +0200

    añadido typos commitee

--- committee_list.h
+++ committee_list.h
@@ -1,5 +1,5 @@
 /*
-* TITLE: PROGRAMMING II LABS
+ * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 2
  * AUTHOR 1: ***************************** LOGIN 1: **********
  * AUTHOR 2: ***************************** LOGIN 2: **********
@@ -7,7 +7,28 @@
  * DATE: ** / ** / **
  */
 
-#include "types.h"
 #include "project_list.h"
+#include "types.h"
 
 /* Write your code here... */
+#define MAX_COMITES 10
+#define NULLC -1
+typedef struct {
+  tCommitteeName committeeName;
+  tNumVotes totalEvaluators;
+  tNumVotes validVotes;
+  tNumVotes nullVotes;
+  tListP projectList;
+} tItemC;
+
+struct Lista {
+  tItemC data[MAX_COMITES];
+  int last;
+};
+
+typedef int tPosC;
+typedef tPosC tListC;
+
+
+
+
