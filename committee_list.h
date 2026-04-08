cd739a217daa71117b508d9ba247601d0cacf670
MateoGonzalezLourido <mlourido1467@gmail.com>
2026-04-08 20:51:10 +0200

    comentado .h

--- committee_list.h
+++ committee_list.h
@@ -32,27 +32,70 @@ typedef Lista tListC;
 // funciones
 
 // funciones del tipo tList
-void createEmptyListC(tListC *L); // crear lista (devuelve un tList), (void)
-                                  // indica "no recibe argumentos"
-bool isEmptyListC(tListC L);      // vacia?; recibe la lista
-tPosC firstC(tListC L); // devolver primer indice ocupado;recibe la lista
-tPosC lastC(tListC L);  // devolver ultimo indice ocupado; recibe la lista
-tPosC nextC(
-    tPosC indice,
-    tListC L); // devolver siguiente indice ocupado;recibe indice y lista
-tPosC previousC(
-    tPosC indice,
-    tListC L); // devolver anterior indice ocupado; recibe indice y lista
-bool insertItemC(tItemC data,
-                 tListC *L); // añadir elemento;recibe info insertar, indice
-                             // donde se cambia, lista usar
-void deleteAtPositionC(tPosC indice,
-                       tListC *L);       // borrar proyecto; recibe indice donde
-                                         // borrar y la lista donde borrar
-tItemC getItemC(tPosC indice, tListC L); // recuperar info proyecto
-void updateItemC(tItemC data, tPosC indice,
-                 tListC *L); // modificar lista; recibe cambiar, indice lista
-                             // actualizar, lista usar
-tPosC findItemC(
-    tProjectName data,
-    tListC L); // buscar indice proyecto; recibe info proyecto, lista usar
+
+void createEmptyListC(tListC *L);         /*Crea una lista (vacia)
+        recibe de entradas la memoria de la lista (ya inicializada)
+        Salidas: L pasa a ser null
+        No hay condiciones ni postcondiciones
+        */
+bool isEmptyListC(tListC L);              /*comprueba si la lista esta vacia
+              recibe de entradas el valor de la lista
+              salidas: TRUE:vacia, FALSE:no vacia
+              no hay precondiciones ni postcondiciones
+              */
+tPosC firstC(tListC L);                   /*devolver primer nodo ocupado
+                 recibe de entradas el valor de la lista
+                 salida: indice del primer elemento de la lista
+                 precondicion: lista no vacia
+                 postcondicion: no hay
+                 */
+tPosC lastC(tListC L);                    /*devolver ultimo nodo ocupado
+                  recibe de entradas el valor de la lista
+                  salida: indice del ultimo elemento de la lista
+                  precondicion: lista no vacia
+                  postcondicion: no hay
+                  */
+tPosC nextC(tPosC indice, tListC L);      /*devolver siguiente nodo ocupado;
+       recibe de entradas el valor de la lista y el indice al que buscaremos su
+       siguiente      salida: indice del siguiente elemento de la lista / null
+       precondicion: indice recibido valido
+       postcondicion: no hay
+       */
+tPosC previousC(tPosC indice, tListC L);  /*devolver anterior nodo ocupado;
+      recibe de entradas el valor de la lista y el indice al que buscaremos su
+      anterior  salida: indice del elemento anterior de la lista / null
+      precondicion: indice recibido valido
+      postcondicion: no hay
+      */
+bool insertItemC(tItemC data, tListC *L); /*añadir elemento a la lista
+recibe de entradas la p.memoria de la lista y el item que añadiremos
+salida: true/false segun saliese bien o no
+precondicion: indice donde insertar recibido valido
+postcondicion: Las posiciones de los elementos de la lista posteriores a la del
+elemento insertado pueden haber variado
+*/
+void deleteAtPositionC(tPosC indice, tListC *L); /*borrar proyecto
+recibe de entradas el indice a borrar y la p.memoria de la lista
+salida: la lista cambiada
+precondicion: La posicion indicada es una posicion valida en la lista
+postcondicion: Las posiciones de los elementos de la lista posteriores a la de
+la            posicion eliminada pueden haber variado.
+*/
+tItemC getItemC(tPosC indice, tListC L); /*recuperar info proyecto de un nodo de
+la lista recibe de entradas el valor de la lista y el indice del que obtendremos
+su info salida: el objeto pedido precondicion: indice recibido valido
+postcondicion: no hay
+*/
+void updateItemC(tItemC data, tPosC indice, tListC *L); /*actualizar un nodo de
+la lista recibe de entradas la nueva data que pondremos, el indice de la lista y
+la p.memoria de la lista
+salida: void
+precondicion: indice recibido valido
+postcondicion: El orden de los elementos de la lista no se ve modificado
+*/
+tPosC findItemC(tProjectName data, tListC L); /*buscar indice proyecto;
+recibe de entradas el nombre del proyecto y el valor de la lista
+salida: el indice del nodo / null
+precondicion: no hay
+postcondicion: no hay
+*/
